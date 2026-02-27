191c86e0f196affc43c421845c7e476b32b322cc
Tatu1984 <saddygrouppie@gmail.com>
2026-02-27 14:59:55 +0530

    fixes

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -175,7 +175,7 @@ export function Header({ onMobileMenuToggle }: HeaderProps) {
       </Popover>
 
       {/* Right section */}
-      <div className="flex items-center gap-2">
+      <div className="ml-auto flex items-center gap-2">
         {/* Notifications */}
         <Popover>
           <PopoverTrigger asChild>
